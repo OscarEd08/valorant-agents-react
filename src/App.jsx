7d4487a528c63afe1034f140a18f700547e78cc7
Oscar Pairazaman <70730776+OscarEd08@users.noreply.github.com>
2023-03-16 09:20:41 -0500

    Actualizar App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import AgentList from './components/AgentList';
 import DarkModeToggle from './components/DarkModeToggle';
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(true);
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
   useEffect(() => {
     isDarkMode ? document.body.classList.add("dark") : document.body.classList.remove("dark");    
