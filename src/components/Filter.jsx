961c72845c1a3d373d37ab36398717a6fc6097c7
Oscar Pairazaman <70730776+OscarEd08@users.noreply.github.com>
2023-03-25 17:32:21 -0500

    dandole más ancho y menos gap a los botones de filtro en pantallas >1280px

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -9,12 +9,12 @@ function Filter(props) {
     .filter((name, index, arr) => arr.indexOf(name) === index);
 
   return (
-    <div className="grid grid-cols-3 sm:grid-cols-5 my-10 mx-10 place-items-center gap-x-16 gap-y-5 sm:gap-8 lg:gap-10 xl:gap-28 2xl:gap-40">
+    <div className="grid grid-cols-3 sm:grid-cols-5 my-10 mx-10 place-items-center gap-x-16 gap-y-5 sm:gap-8 lg:gap-10 xl:gap-0">
       {nameRoles.map((role, index) => (
         <button
           key={index}
           onClick={() => handleRoleSelect(role)}
-          className={`text-sm md:text-base py-3 w-28 lg:w-44 lg:py-4 font-semibold rounded-full border hover:text-white hover:bg-gray-800 dark:text-white dark:hover:bg-gray-900 hover:border-transparent focus:outline-none ${
+          className={`text-sm md:text-base py-3 w-28 lg:w-44 lg:py-4 xl:w-48 2xl:w-52 font-semibold rounded-full border hover:text-white hover:bg-gray-800 dark:text-white dark:hover:bg-gray-900 hover:border-transparent focus:outline-none ${
             selectedRole === role
               ? "bg-gray-800 border-gray-800 text-white dark:bg-gray-900 dark:border-gray-900"
               : "text-gray-800 border-gray-200"
@@ -26,7 +26,7 @@ function Filter(props) {
       ))}
       <button
         onClick={() => handleRoleSelect("")}
-        className={`ml-32 sm:ml-0 text-xs md:text-base py-3 w-28 lg:w-44 lg:py-4 font-semibold rounded-full border hover:border-transparent focus:outline-none ${
+        className={`ml-32 sm:ml-0 text-xs md:text-base py-3 w-28 lg:w-44 xl:w-48 2xl:w-52 lg:py-4 font-semibold rounded-full border hover:border-transparent focus:outline-none ${
           selectedRole === ""
             ? "text-white bg-gray-800 border-gray-800 dark:bg-gray-900 dark:border-gray-900"
             : "text-gray-800 hover:text-white hover:bg-gray-800 dark:text-white dark:hover:bg-gray-900 "
